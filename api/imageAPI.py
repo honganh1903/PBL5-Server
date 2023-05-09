0a053cd0f9906f7de03af4557e8c174050e417eb
bongmaxuyentuong1 <bongmaxuyentuong1@gmail.com>
2023-05-10 00:19:53 +0700

    update

--- api/imageAPI.py
+++ api/imageAPI.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify, send_file
 from firebase_admin import storage, db
 from datetime import datetime, timedelta
-
+from ultralytics import YOLO
 
 imageAPI = Blueprint('imageAPI', __name__)
 
+model = YOLO('D:/PBL5/Model/best.pt')
 
 @imageAPI.route('/', methods=['POST'])
 # def upload_image():
@@ -28,25 +29,26 @@ imageAPI = Blueprint('imageAPI', __name__)
 def upload_image_and_create_item():
     # Lấy file ảnh từ form data
     image = request.files['image']
-    image_data = image.read()
-    image_name = "Fire_Detection/" + image.filename
-    image_content_type = image.content_type
-
-    # Tải ảnh lên Firebase Storage
-    bucket = storage.bucket()
-    blob = bucket.blob(image_name)
-    blob.upload_from_string(image_data, content_type=image_content_type)
-    url = blob.generate_signed_url(
-        version='v4',
-        expiration=timedelta(minutes=15),
-        method='GET'
-    )
+    results = model.predict(source = image)
+    if results[0].boxes.conf[results[0].boxes.cls == 0].item() > 0.7:
+        print(results[0].boxes.cls)
+        # Tải ảnh lên Firebase Storage
+        image_data = image.read()
+        image_name = "Fire_Detection/" + image.filename
+        image_content_type = image.content_type
+        bucket = storage.bucket()
+        blob = bucket.blob(image_name)
+        blob.upload_from_string(image_data, content_type=image_content_type)
+        url = blob.generate_signed_url(
+            version='v4',
+            expiration=timedelta(minutes=15),
+            method='GET'
+        )
 
     # Lấy dữ liệu từ form data và thêm thuộc tính 'date' vào
     item = {}
     item['date'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     item['imageUrl'] = url
-    item['status'] = True
 
     # Tải dữ liệu lên Firebase Realtime Database
     ref = db.reference('user')
